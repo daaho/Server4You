0840e919789e2a12b3c5433e95384bb8eab1bbbc
Stefan Hoenes <stefan@hoenes-gerlingen.de>
2020-12-20 17:34:47 +0100

    nginx-proxy

--- 01 nginx-proxy/create_docker_environment.sh
+++ 01 nginx-proxy/create_docker_environment.sh
@@ -2,6 +2,7 @@ docker network create proxy
 docker network create backend
 docker network create backend-test
 docker network create monitoring
+docker network create logging
 
 docker volume create conf.d
 docker volume create vhost.d
